2f0a6adf796da702d54853644fecb3446491e02d
abeytaadam <measalion@gmail.com>
2015-11-07 21:38:03 -0800

    Almost done. Needs restart button

--- main.js
+++ main.js
@@ -1,77 +1,133 @@
 $(document).ready(function() {
 
-console.log('JSLOADED');
+	console.log('JSLOADED');
+
+	//Click events
+	var clicks = 0;
+
+	//Countdown to game start
+	var count = 3;
+	var timer = setInterval(function() {
+		handleTimer(count);
+	}, 1000);
+
+	//Player 1 keypresses
+	var countA = 0;
+	var countS = 0;
+	var countD = 0;
+	var countF = 0;
+
+	//Player 2 keypresses
+	var countJ = 0;
+	var countK = 0;
+	var countL = 0;
+	var countSC = 0;
 
 
-$('#gameClick').click(function() {
-	$('#gameClick').hide();
-	$('.gameTitle').hide();
-	$('.row').show();
-	$('.gameBoard').show();
 
+	$('#gameClick').click(function() {
+		$('.gameTitle').hide();
+		clicks++;
+		if (clicks == 1) {
+			$('.gameInst').show();
+			$('.row').show();
+		} else if (clicks > 1) {
+			$('#gameClick').hide();
+			$('.row').show();
+			$('.gameBoard').show();
+			$('.gameInst').hide();
+			$('.players').hide();
+			$('#countDown').show();
+		}
+	});
+
+	$('.gameInst').hide();
+	$('.row').hide();
+	$('.gameBoard').hide();
+	$('#countDown').hide();
+
+
+
+	function handleTimer() {
+		if (count === 0) {
+			clearInterval(timer);
+			startGame();
+		} else {
+			$('#countDown').html(count);
+			count--;
+		}
+	}
+
+	function stopGame() {
+		if (countF == 30) {
+			$(document).off('keydown');
+			$("#gb1").text("Player 1 wins!");
+			document.getElementById("#p2").src = "IMGs/sleepyCatlose.gif";
+		} else if (countSC == 30){
+			$(document).off('keydown');
+			$("#gb2").text("Player 2 wins!");
+			document.getElementById("#p1").src = "IMGs/sleepyCatlose.gif";
+			
+		}
+	}
+
+	function startGame() {
+		$('#countDown').hide();
+		//Allows for only successive keypresses (a>s>d>f)
+		$(document).keydown(function(event) {
+
+			if (event.which == 65 && (countA == countF)) {
+				countA++;
+				$("#gb1").text("S");
+			} else if (event.which == 83 && (countA > countS)) {
+				countS++;
+				$("#gb1").text("D");
+			} else if (event.which == 68 && (countS > countD)) {
+				countD++;
+				$("#gb1").text("F");
+			} else if (event.which == 70 && (countD > countF)) {
+				countF++;
+				$("#gb1").text("A");
+			}
+			console.log(countA, countS, countD, countF);
+
+			//Shows progress of race through cat animations Player 1
+			if (countF == 10) {
+				document.getElementById("#p1").src = "IMGs/sleepyCat2.gif";
+			} else if (countF == 20) {
+				document.getElementById("#p1").src = "IMGs/sleepyCat3.gif";
+			} else if (countF == 30) {
+				document.getElementById("#p1").src = "IMGs/sleepyCat4.gif";
+				stopGame();
+			}
+
+			//Allows for only successive keypresses (j>k>l>;)
+			if (event.which == 74 && (countJ == countSC)) {
+				countJ++;
+				$("#gb2").text("K");
+			} else if (event.which == 75 && (countJ > countK)) {
+				countK++;
+				$("#gb2").text("L");
+			} else if (event.which == 76 && (countK > countL)) {
+				countL++;
+				$("#gb2").text(";");
+			} else if (event.which == 186 && (countL > countSC)) {
+				countSC++;
+				$("#gb2").text("J");
+			}
+			console.log(countJ, countK, countL, countSC);
+
+
+			//Shows progress of race through cat animations Player 2
+			if (countSC == 10) {
+				document.getElementById("#p2").src = "IMGs/sleepyCat2.gif";
+			} else if (countSC == 20) {
+				document.getElementById("#p2").src = "IMGs/sleepyCat3.gif";
+			} else if (countSC == 30) {
+				document.getElementById("#p2").src = "IMGs/sleepyCat4.gif";
+				stopGame();
+			}
+
+		});
+	}
 });
-
-$('.row').hide();
-$('.gameBoard').hide();
-
-//Player 1 keypresses
-var countA = 0;
-var countS = 0;
-var countD = 0;
-var countF = 0;
-
-//Player 2 keypresses
-var countJ = 0;
-var countK = 0;
-var countL = 0;
-var countSC = 0;
-
-//Allows for only successive keypresses (a>s>d>f & j>k>l>;)
-$( document ).keydown(function( event ) {
-
-  if ( event.which == 65 && (countA == countF)) {
-   countA++;
-  } else if (event.which == 83 && (countA > countS)) {
-  	countS++;
-  } else if (event.which == 68 && (countS > countD)) {
-  	countD++;
-  } else if (event.which == 70 && (countD > countF)) {
-  	countF++;
-  }
-  console.log(countA, countS, countD, countF);
-
-  if (countF == 10) {
-	document.getElementById("#p1").src="IMGs/sleepyCat2.gif";
-} else if (countF == 20) {
-	document.getElementById("#p1").src="IMGs/sleepyCat3.gif";
-} else if (countF == 30) {
-	document.getElementById("#p1").src="IMGs/sleepyCat4.gif";
-}
-
-    if ( event.which == 74 && (countJ == countSC)) {
-   countJ++;
-  } else if (event.which == 75 && (countJ > countK)) {
-  	countK++;
-  } else if (event.which == 76 && (countK > countL)) {
-  	countL++;
-  } else if (event.which == 186 && (countL > countSC)) {
-  	countSC++;
-  }
-  console.log(countJ, countK, countL, countSC);
-
-    if (countSC == 10) {
-	document.getElementById("#p2").src="IMGs/sleepyCat2.gif";
-} else if (countSC == 20) {
-	document.getElementById("#p2").src="IMGs/sleepyCat3.gif";
-} else if (countSC == 30) {
-	document.getElementById("#p2").src="IMGs/sleepyCat4.gif";
-}
-
-});
-
-});
-// function aOn () {
-// 	if ()
-// }
-
-// });
